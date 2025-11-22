c2b52fa4354238675b9040270735f02537f36421
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2025-11-22 19:02:44 +0700

    update meong-smp/bp/scripts/index.js

--- meong-smp/bp/scripts/index.js
+++ meong-smp/bp/scripts/index.js
@@ -27,8 +27,10 @@ world.afterEvents.playerJoin.subscribe((event) => {
 
 	if (!db.get("isMember", player.name)) {
 		db.set("isMember", true, player.name);
-		dn.ser("nirvane.amount", configs.gameplay.initial, player.name);
-		dn.ser("nirvane.maxAmount", configs.gameplay.initial, player.name);
+		db.set("nirvane.amount", configs.gameplay.initial, player.name);
+		db.set("nirvane.maxAmount", configs.gameplay.initial, player.name);
+		db.set("nirvane.regenAmount", configs.gameplay.regenAmount, player.name);
+		db.set("nirvane.regenTime", configs.gameplay.regenTime, player.name);
 		player.sendMessage(`§2§l» §r§aWelcome §7${player.name}! §fYou're new here!`);
 	} else {
 		player.sendMessage(`§2§l» §r§aWelcome back §7${player.name}`);
