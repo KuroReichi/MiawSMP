ecf1f7474b0ebe0fbae4ad41beb607bd4c22c1ae
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2025-11-22 18:57:25 +0700

    update meong-smp/bp/scripts/index.js

--- meong-smp/bp/scripts/index.js
+++ meong-smp/bp/scripts/index.js
@@ -18,6 +18,7 @@
 
 import { world, system } from "@minecraft/server";
 import { uiManager, ActionFormData, ModalFormData, MessageFormData } from "@minecraft/server-ui";
+import { configs } from "./configs.js"
 import { db } from "@minecraft/database.js";
 
 world.afterEvents.playerJoin.subscribe((event) => {
@@ -25,6 +26,8 @@ world.afterEvents.playerJoin.subscribe((event) => {
 	const player = event.player;
 
 	if (!db.get("isMember", player.name)) {
+		db.set("isMember", true, player.name);
+		dn.ser("nirvane.amount", )
 		player.sendMessage(`§2§l» §r§aWelcome §7${player.name}! §fYou're new here!`);
 	} else {
 		player.sendMessage(`§2§l» §r§aWelcome back §7${player.name}`);
