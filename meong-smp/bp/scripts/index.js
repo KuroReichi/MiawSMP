9de3b3bd6369d710c88c5469dea56cf82a60e332
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2025-11-23 17:07:22 +0700

    update meong-smp/bp/scripts/index.js

--- meong-smp/bp/scripts/index.js
+++ meong-smp/bp/scripts/index.js
@@ -48,8 +48,9 @@ world.afterEvents.playerSpawn.subscribe((event) => {
 });
 
 world.afterEvents.playerBreakBlock.subscribe((event) => {
-	db.incValue("block.break", 1, event.player.name)
+	db.incValue("block.break", 1, event.player.name);
+	console.info(db.get(block.break))
 });
 world.afterEvents.playerPlaceBlock.subscribe((event) => {
-	db.incValue("block.place", 1, event.player.name)
+	db.incValue("block.place", 1, event.player.name);
 });
