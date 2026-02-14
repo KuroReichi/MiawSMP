669b872b2838a07df11ccbccbe1511272f34188e
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-02-15 01:51:43 +0700

    update scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -16,14 +16,23 @@
 //░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 //░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-import { ItemStack, system, world } from "@minecraft/server";
+import { ItemStack, ItemType, system, world } from "@minecraft/server";
 import { uiManager, ActionFormData, ModalFormData, MessageFormData } from "@minecraft/server-ui";
 
 import { db } from "@minecraft/database.js";
 import { configs, PlayerClass } from "./configs.js";
+import { 
 
 world.afterEvents.worldLoad.subscribe(() => {
 	console.info("MIAW's Core Loaded");
 	db.set("server.dateCreated", new Date().valueOf(), "global", false);
 	db.set("server.uptime", new Date().valueOf(), "global", true);
+});
+
+world.beforeEvents.playerInteractWithEntity.subscribe((event) => {
+	if(event.itemStack.typeId === undefined) {
+		
+	} else if(event.itemStack.typeId === "minecraft:name_tag") {
+		
+	}
 });
