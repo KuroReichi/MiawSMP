b04609d2b2e53dd7f9b406a7005a7bdbb0f821c5
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-02-15 01:13:58 +0700

    create scripts/interface.js

---
+++ scripts/interface.js
@@ -0,0 +1,10 @@
+import { world, system } from "@minecraft/server";
+import {
+	ActionFormData,
+	ModalFormData,
+	MessageFormData
+} from "@minecraft/server-ui";
+import { db } from "@minecraft/database.js";
+import { configs } from "./configs.js";
+
+export const UI = {};
