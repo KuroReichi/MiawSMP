f0fc823bd2598dc32cc0e7b8a05b81ab0c38c1b5
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-08 07:52:05 +0700

    update scripts/@cores/command-builder.js

--- scripts/@cores/command-builder.js
+++ scripts/@cores/command-builder.js
@@ -4,7 +4,6 @@
  *		@param {object} data
  **/
 export class CommandBuilder {
-	constructor(data) {
-		
-	}
+	constructor(data) {}
 }
+//=======================================================================================
