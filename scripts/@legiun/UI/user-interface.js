a48b0889a2a278fc86de4799f575a3b7ffff9b21
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-09 04:27:43 +0700

    Delete the file at 'scripts/@legiun/UI/user-interface.js'

--- scripts/@legiun/UI/user-interface.js
+++
@@ -1,6 +0,0 @@
-import { world, system } from "@minecraft/server";
-import { ActionFormData, ModalFormData, MessageFormData } from "@minecraft/server-ui";
-import database from "./../database.js";
-import { configs } from "./../../configs.js";
-
-export const UI = {};
