f4ffdff26b5f68c56d965d63d4febb4f3dbf9be6
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-09 00:50:20 +0700

    update scripts/@legiun/frameworks/utility.js

---
+++ scripts/@legiun/frameworks/utility.js
@@ -0,0 +1,20 @@
+/**
+ *	@name metricNumber
+ *	@description - Mengonversi angka menjadi format singkatan metrik (k, M, G, dst).
+ *	@function - Memformat angka besar menjadi string yang lebih ringkas dan mudah dibaca.
+ *		@param {number} value - Angka yang akan diformat.
+ *		@param {number} decimals - Jumlah angka di belakang koma (default: 2).
+ **/
+export function metricNumber(value, decimals = 2) {
+	if (value === 0) return "0";
+	const absValue = Math.abs(value);
+	if (absValue < 1000) return value.toString();
+	const k = 1000;
+	const dm = decimals < 0 ? 0 : decimals;
+	const sizes = ["", "k", "M", "G", "T", "P", "E", "Z", "Y"];
+	let i = Math.floor(Math.log(absValue) / Math.log(k));
+	if (i >= sizes.length) i = sizes.length - 1;
+	const formattedNumber = parseFloat((absValue / Math.pow(k, i)).toFixed(dm));
+	return (value < 0 ? "-" : "") + formattedNumber + sizes[i];
+}
+//=======================================================================================
