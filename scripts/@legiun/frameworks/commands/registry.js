c09ea920c05abdfe0c7df34fc7b024310465bcc3
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-09 15:12:48 +0700

    update scripts/@legiun/frameworks/commands/registry.js

--- scripts/@legiun/frameworks/commands/registry.js
+++ scripts/@legiun/frameworks/commands/registry.js
@@ -9,28 +9,5 @@ export function getCommands() {
 
 //===================================================================================
 
-export function registerCommand(command) {
-	const index = registry.findIndex((c) => c.name === command.name);
-	if (index !== -1) registry.splice(index, 1);
-	registry.push(command);
-	registry.sort((a, b) => a.name.localeCompare(b.name));
-}
-
-//===================================================================================
-
-export function buildCommands(apiRegistry) {
-	for (const command of registry) {
-		apiRegistry.registerCommand(
-			{
-				name: command.name,
-				description: command.description ?? "",
-				permissionLevel: command.permissionLevel ?? 0
-			},
-			(root) => {
-				command.build?.(root);
-			}
-		);
-	}
-}
 
 //===================================================================================
