827816a0731a6d65511c68292a569e1a90b92117
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-10 01:07:55 +0700

    update scripts/@legiun/frameworks/commands/handler.js

--- scripts/@legiun/frameworks/commands/handler.js
+++ scripts/@legiun/frameworks/commands/handler.js
@@ -2,7 +2,9 @@ import { world } from "@minecraft/server";
 import { configs } from "./../../../configs.js";
 import { getCommand } from "./registry.js";
 
+if(configs.commandPrefix)
 world.beforeEvents.chatSend.subscribe((event) => {
-	if(event.message.startsWith()) {
+	if (event.message.startsWith(configs.commandPrefix)) {
+		
 	}
 });
