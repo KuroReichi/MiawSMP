68653256dff7eb042065439c624cd6aa725c585d
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-10 01:12:12 +0700

    update scripts/@legiun/frameworks/commands/handler.js

--- scripts/@legiun/frameworks/commands/handler.js
+++ scripts/@legiun/frameworks/commands/handler.js
@@ -3,10 +3,10 @@ import { configs } from "./../../../configs.js";
 import { getCommand } from "./registry.js";
 
 if (configs.commandPrefix.includes("/")) {
-	console.info(`§4[§cError§4]§7: §g(§eat §fhandler.js§g)\n    §9pPrefix cannot contains slash "/"§3..`)
+	console.info(`§4[§cError§4]§7: §g(§eat §fhandler.js§g)\n    §9pPrefix cannot contain a slash "/"§3..`);
 	return;
 }
-	world.beforeEvents.chatSend.subscribe((event) => {
-		if (event.message.startsWith(configs.commandPrefix)) {
-		}
-	});
+world.beforeEvents.chatSend.subscribe((event) => {
+	if (event.message.startsWith(configs.commandPrefix)) {
+	}
+});
