153ab854256e85fba5ceb2f3904f01ba5f486a0f
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-10 01:17:09 +0700

    update scripts/@legiun/frameworks/commands/handler.js

--- scripts/@legiun/frameworks/commands/handler.js
+++ scripts/@legiun/frameworks/commands/handler.js
@@ -5,11 +5,10 @@ import { getCommand } from "./registry.js";
 if (configs.commandPrefix.includes("/")) {
 	console.info(`§4[§cError§4]§7: §g(§eat §fhandler.js§g)\n    §9pPrefix cannot contain a slash "/"§3..`);
 	configs.commandPrefix = "!";
-	console.info(`§2[§aInfo§2]§7: §fChange the prefix to standard characters, now the prefix is "!".`)
+	console.info(`§2[§aInfo§2]§7: §fChange the prefix to standard characters, now the prefix is "!".`);
 }
 world.beforeEvents.chatSend.subscribe((event) => {
 	if (event.message.startsWith(configs.commandPrefix)) {
 	} else {
-		
 	}
 });
