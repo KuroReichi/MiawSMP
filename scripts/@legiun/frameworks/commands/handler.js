d5bda7ac97cec2451b35f45922e53eaeabfafd5b
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-10 19:48:45 +0700

    update scripts/@legiun/frameworks/commands/handler.js

--- scripts/@legiun/frameworks/commands/handler.js
+++ scripts/@legiun/frameworks/commands/handler.js
@@ -4,11 +4,12 @@ import { getCommand } from "./registry.js";
 
 if (configs.commandPrefix.startsWith("/")) {
 	console.info(`§4[§cERROR§4]§7: §eat §ghandler.js`);
-	console.info(`">> ${configs.commandPrefix.substr(configs.commandPrefix.indexOf(query), 1)} << ${configs.commandPrefix.slice(configs.commandPrefix.indexOf(query) + 1, 3)}..."`)
+	console.info(`">> ${configs.commandPrefix.substr(configs.commandPrefix.indexOf(query), 1)} << ${configs.commandPrefix.slice(configs.commandPrefix.indexOf(query) + 1, 3)}..."`);
 	configs.commandPrefix = "!";
 	console.info(`§2[§aINFO§2]§7: §fChange the prefix to standard characters, now the prefix is "!".`);
 }
- 
+
+      
 world.beforeEvents.chatSend.subscribe((event) => {
 	if (event.message.startsWith(configs.commandPrefix)) {
 		event.cancel = true;
