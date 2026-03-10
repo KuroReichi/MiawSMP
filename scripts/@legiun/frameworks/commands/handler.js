81948e63c02d24301e4421803c7b56d7bb1c19c1
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-10 19:47:54 +0700

    update scripts/@legiun/frameworks/commands/handler.js

--- scripts/@legiun/frameworks/commands/handler.js
+++ scripts/@legiun/frameworks/commands/handler.js
@@ -3,16 +3,16 @@ import { configs } from "./../../../configs.js";
 import { getCommand } from "./registry.js";
 
 if (configs.commandPrefix.startsWith("/")) {
-	console.info(`§4[§cError§4]§7: §g(§eat §fhandler.js§g)\n    §9pPrefix cannot contain a slash "/"§3..`);
+	console.info(`§4[§cERROR§4]§7: §eat §ghandler.js`);
+	console.info(`">> ${configs.commandPrefix.substr(configs.commandPrefix.indexOf(query), 1)} <<   ${configs.commandPrefix.slice(configs.commandPrefix.indexOf(query) + 1, 3)}..."`;)
 	configs.commandPrefix = "!";
-	console.info(`§2[§aInfo§2]§7: §fChange the prefix to standard characters, now the prefix is "!".`);
+	console.info(`§2[§aINFO§2]§7: §fChange the prefix to standard characters, now the prefix is "!".`);
 }
 
 world.beforeEvents.chatSend.subscribe((event) => {
 	if (event.message.startsWith(configs.commandPrefix)) {
 		event.cancel = true;
 		if (!configs.server.staff.includes(event.sender.name)) console.info(`${event.sender.name}: ${event.message}`);
-		
 	} else {
 		event.cancel = true;
 		console.info(`${event.sender.name}: ${event.message}`);
