cae22f169ca349e683ce24789dbd62e932c79ec1
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-10 19:27:51 +0700

    update scripts/@legiun/frameworks/commands/handler.js

--- scripts/@legiun/frameworks/commands/handler.js
+++ scripts/@legiun/frameworks/commands/handler.js
@@ -2,7 +2,7 @@ import { world } from "@minecraft/server";
 import { configs } from "./../../../configs.js";
 import { getCommand } from "./registry.js";
 
-if (configs.commandPrefix.includes("/")) {
+if (configs.commandPrefix.startsWith("/")) {
 	console.info(`§4[§cError§4]§7: §g(§eat §fhandler.js§g)\n    §9pPrefix cannot contain a slash "/"§3..`);
 	configs.commandPrefix = "!";
 	console.info(`§2[§aInfo§2]§7: §fChange the prefix to standard characters, now the prefix is "!".`);
