f7b3ddbeca01a3163a52bc64d2b884687f7a9d8c
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-11 16:53:51 +0700

    update scripts/@legiun/frameworks/commands/handler.js

--- scripts/@legiun/frameworks/commands/handler.js
+++ scripts/@legiun/frameworks/commands/handler.js
@@ -8,6 +8,7 @@ if (prefix.startsWith("/")) {
 	console.info(`§4[§cERROR§4]§7: §eat §ghandler.js`);
 	console.info(`    The prefix: ">> ${prefix.substr(prefix.indexOf("/"), 1)} << ${prefix.slice(prefix.indexOf("/") + 1, 3)}${prefix.length >= 3 ? "..." : ""}", cannot starts with a slash (/).`);
 	configs.commandPrefix = "!";
+	prefix = "!";
 	console.info(`§2[§aINFO§2]§7: §fChanged the prefix to standard character, now the prefix is "!".`);
 }
 
