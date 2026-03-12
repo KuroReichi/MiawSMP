255caae4cd0fc68c413908f4095f0019607df72d
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-12 18:51:11 +0700

    update scripts/@legiun/frameworks/commands/registry.js

--- scripts/@legiun/frameworks/commands/registry.js
+++ scripts/@legiun/frameworks/commands/registry.js
@@ -7,6 +7,10 @@ export function getCommands() {
 	return registry;
 }
 
+export function registerCommand(command) {
+	registry.push(command);
+	console.info(`[Push]: ${command.name} has been added`);
+}
 //===================================================================================
 
 async function pendingCommand(command) {
