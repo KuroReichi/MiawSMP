9c50bffc302cf5dff6c988cc1e02d21829e0cf56
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-16 07:31:42 +0700

    update scripts/@legiun/frameworks/syncTime.js

--- scripts/@legiun/frameworks/syncTime.js
+++ scripts/@legiun/frameworks/syncTime.js
@@ -1,37 +1,58 @@
 import { world, system } from "@minecraft/server";
 
+const TIMEZONE = "Asia/Jakarta";
+
+/**
+ * --------------------------------------------------
+ * @name getJakartaTime
+ * @description Get current Jakarta time using Date API
+ * @function
+ * --------------------------------------------------
+ */
+function getJakartaTime() {
+	const date = new Date();
+	const jakarta = new Date(
+		date.toLocaleString("en-US", { timeZone: TIMEZONE })
+	);
+
+	return {
+		hour: jakarta.getHours(),
+		minute: jakarta.getMinutes(),
+		second: jakarta.getSeconds()
+	};
+}
+
 /**
  * --------------------------------------------------
  * @name getMinecraftTimeFromIRL
- * @description Convert IRL time to Minecraft ticks
+ * @description Convert IRL time (Jakarta) into Minecraft ticks
  * @function
  * --------------------------------------------------
  */
 function getMinecraftTimeFromIRL() {
-	const now = Temporal.Now.zonedDateTimeISO("Asia/Jakarta");
-
-	const hour = now.hour;
-	const minute = now.minute;
-	const second = now.second;
-
+	const { hour, minute, second } = getJakartaTime();
 	const irlHours = hour + minute / 60 + second / 3600;
-
 	let mcHours = irlHours - 6;
 	if (mcHours < 0) mcHours += 24;
-
 	return Math.floor(mcHours * 1000);
 }
 
 /**
  * --------------------------------------------------
  * @name startWorldTimeSync
- * @description Sync Minecraft world time with IRL time
+ * @description Sync Minecraft world time with Jakarta time
+ * @function
  * --------------------------------------------------
  */
 export function startWorldTimeSync() {
+
 	system.runInterval(() => {
+
 		const ticks = getMinecraftTimeFromIRL();
 
-		world.getDimension("overworld").runCommand(`time set ${ticks}`);
+		world.getDimension("overworld").runCommand(
+			`time set ${ticks}`
+		);
+
 	}, 200); // update tiap 10 detik
 }
