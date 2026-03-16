7d6829b1b9ce3ce110541bb30ee767df506dafb3
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-16 18:35:30 +0700

    update scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -19,10 +19,16 @@
 
 import "./@legiun/frameworks/commands/handler.js";
 import "./@legiun/frameworks/commands/loader.js";
+import database from "./@legiun/database.js";
 import { startWorldTimeSync } from "./@legiun/frameworks/syncTime.js";
 import { world, system } from "@minecraft/server";
 
 world.afterEvents.worldLoad.subscribe(() => {
+	database.set("server.startDate", new Date().valueOf(), "server", false);
+	database.set("server.lastStartDate", new Date().valueOf(), "server", true);
+	database.set("timezone.location", "Asia/Jakarta", "global", false);
+	
 	startWorldTimeSync();
 });
+
 //===================================================================================
