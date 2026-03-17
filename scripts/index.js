884543278fe893f0526dfd687366be1d04710cea
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-18 06:35:11 +0700

    update scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -19,32 +19,17 @@
 
 import "./@legiun/frameworks/commands/handler.js";
 import "./@legiun/frameworks/commands/loader.js";
+import "./@legiun/frameworks/chat.js";
 import database from "./@legiun/database.js";
 import { startWorldTimeSync } from "./@legiun/frameworks/syncTime.js";
 import { world, system } from "@minecraft/server";
 
-system.beforeEvents.startup.subscribe((event) => {
-	const registry = event.customCommandRegistry;
-	registry.registerCommand({
-		name: "chat",
-		description: "Open chat UI",
-		permissionLevel: 0
-	}, (origin, args) => {
-
-		const player = origin.sourceEntity;
-
-		if (!player) return;
-		system.run(() => {
-			openHome(player);
-		});
-	});
-});
 
 world.afterEvents.worldLoad.subscribe(() => {
 	database.set("server.startDate", new Date().valueOf(), "server", false);
 	database.set("server.lastStartDate", new Date().valueOf(), "server", true);
 	database.set("timezone.location", "Asia/Jakarta", "global", false);
-	
+
 	startWorldTimeSync();
 });
 
