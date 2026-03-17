c98a256ed5a18dfa2e53ee76241324b5831a44a8
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-18 06:35:17 +0700

    update scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -24,7 +24,6 @@ import database from "./@legiun/database.js";
 import { startWorldTimeSync } from "./@legiun/frameworks/syncTime.js";
 import { world, system } from "@minecraft/server";
 
-
 world.afterEvents.worldLoad.subscribe(() => {
 	database.set("server.startDate", new Date().valueOf(), "server", false);
 	database.set("server.lastStartDate", new Date().valueOf(), "server", true);
