ff354d1c751be9490f89f9b30ecd62b9e2b25533
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-18 22:10:40 +0700

    update scripts/@legiun/frameworks/chat.js

--- scripts/@legiun/frameworks/chat.js
+++ scripts/@legiun/frameworks/chat.js
@@ -1,5 +1,10 @@
 import { system } from "@minecraft/server";
 
 system.beforeEvents.startup.subscribe((event) => {
-	
-})
+	event.customCommandRegistry.registerCommand({
+		name: "q:chat",
+		description: "Open Chat GUI",
+		permissionLevel: 0,
+		cheatsRequired: false
+	})
+});
