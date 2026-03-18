92d7a7e09c4b2a7bf16ffb9383f18e6c75550a02
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-19 04:38:36 +0700

    update scripts/@legiun/frameworks/chat.js

--- scripts/@legiun/frameworks/chat.js
+++ scripts/@legiun/frameworks/chat.js
@@ -14,7 +14,7 @@ system.beforeEvents.startup.subscribe((event) => {
 		(origin) => {
 			/**
 			 * @param {CustomCommandOrigin} origin
-			 * @returns { initiator, sourceBlock, sourceEntity, sourceType } CustomCommandOrigin
+			 * @returns { initiator<Entity>, sourceBlock<Block>, sourceEntity<Entity>, sourceType<CustomCommandSource> } CustomCommandOrigin
 			 */
 			if (origin.sourceType !== "Entity" || origin.sourceEntity?.typeId !== "minecraft:player") {
 				console.error("Only player can access this command.");
