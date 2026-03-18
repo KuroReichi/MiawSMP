5a944f55d2f69135e1dda4cff6e79cd343e01a09
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-19 06:24:59 +0700

    update scripts/@legiun/ui/form-builder.js

--- scripts/@legiun/ui/form-builder.js
+++ scripts/@legiun/ui/form-builder.js
@@ -1,8 +1,14 @@
-import { world, system } from "@minecraft/server";
 import { ActionFormData, ModalFormData, MessageFormData } from "@minecraft/server-ui";
+import { system, world } from "@minecraft/server";
 import database from "./../database.js";
 import { configs } from "./../../configs.js";
 
 const Interface = {};
+Interface.messager = {
+	home: function(player) {
+		const v = new ActionFormData();
+		
+	}
+}
 
 export default Interface;
