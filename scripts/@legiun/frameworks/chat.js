94296d79f2473bb8c60a7d1b805a7031916786a4
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-19 04:36:34 +0700

    update scripts/@legiun/frameworks/chat.js

--- scripts/@legiun/frameworks/chat.js
+++ scripts/@legiun/frameworks/chat.js
@@ -14,14 +14,12 @@ system.beforeEvents.startup.subscribe((event) => {
 		(origin) => {
 			/**
 			 * @param {CustomCommandOrigin} origin
-			 * @var {
-			 	* initiator, sourceBlock, sourceEntity, sourceType }
+			 * @var { initiator, sourceBlock, sourceEntity, sourceType } CustomCommandOrigin
 			 */
 			if (origin.sourceType !== "Entity" || origin.sourceEntity?.typeId !== "minecraft:player") {
 				console.error("Only player can access this command.");
 				return;
 			}
-			
 		}
 	);
 });
