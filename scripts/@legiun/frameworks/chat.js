ef3667486f639d38a8fd325d904bc9b545038293
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-19 05:44:28 +0700

    update scripts/@legiun/frameworks/chat.js

--- scripts/@legiun/frameworks/chat.js
+++ scripts/@legiun/frameworks/chat.js
@@ -24,7 +24,7 @@ system.beforeEvents.startup.subscribe((event) => {
 				console.error("Only player can access this command.");
 				return;
 			} // 😹😹😹 pler
-			Interface.messagers.home(origin.sourceEntity);
+			Interface.messager.home(origin.sourceEntity);
 		}
 	);
 });
