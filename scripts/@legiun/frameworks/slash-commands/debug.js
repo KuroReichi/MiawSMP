354e74dc75bfe48df0b2df7173195efd5c4a272b
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-25 15:32:50 +0700

    update scripts/@legiun/frameworks/slash-commands/debug.js

--- scripts/@legiun/frameworks/slash-commands/debug.js
+++ scripts/@legiun/frameworks/slash-commands/debug.js
@@ -1,4 +1,34 @@
+/**
+ * =========================================
+ * @name registerDebugCommand
+ * @description Register custom command "q:debug" untuk membuka UI debug
+ * @function
+ * =========================================
+ */
 import { world, system } from "@minecraft/server";
 import { Interface } from "../../ui/form-builder.js";
 
-system.
+system.beforeEvents.startup.subscribe((event) => {
+	const registry = event.customCommandRegistry;
+
+	registry.registerCommand({
+		name: "q:debug",
+		description: "Open Debug Menu",
+		permissionLevel: 0,
+		overloads: [
+			{
+				parameters: [],
+				handler(origin) {
+					try {
+						const player = origin.sourceEntity;
+						if (!player) return;
+
+						system.run(() => {
+							Interface.debug.main(player);
+						});
+					} catch {}
+				}
+			}
+		]
+	});
+});
