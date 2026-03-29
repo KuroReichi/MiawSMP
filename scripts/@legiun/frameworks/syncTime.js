7d79653f2ec114a4bd339dad8bd66c795f182f04
Kuro Reichi. <95631442+KuroReichi@users.noreply.github.com>
2026-03-29 10:55:42 +0700

    update scripts/@legiun/frameworks/syncTime.js

--- scripts/@legiun/frameworks/syncTime.js
+++ scripts/@legiun/frameworks/syncTime.js
@@ -73,7 +73,7 @@ export function startWorldTimeSync() {
 		const ticks = getMinecraftTimeFromIRL();
 		const days = getDayPassed();
 
-		const finalTime = ticks + Number(days * 24000);
+		const finalTime = ticks + Math.floor(days * 24000);
 
 		world.getDimension("overworld").runCommand(`time set ${finalTime}`);
 	}, 5);
